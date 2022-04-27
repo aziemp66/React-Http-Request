dec4d024c842cc0218b7f4947b392badaf34128a
Azie Melza Pratama <aziemp66@gmail.com>
2022-04-27 23:42:34 +0700

    Adding Get Request Method

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,39 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+	const [movies, setMovies] = useState([]);
 
-  return (
-    <React.Fragment>
-      <section>
-        <button>Fetch Movies</button>
-      </section>
-      <section>
-        <MoviesList movies={dummyMovies} />
-      </section>
-    </React.Fragment>
-  );
+	const fetchMovieHandler = () => {
+		fetch("https://swapi.dev/api/films/")
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				const transformedData = data.results.map((item) => {
+					return {
+						id: item.episode_id,
+						title: item.title,
+						openingText: item.opening_crawl,
+						releaseDate: item.release_date,
+					};
+				});
+				setMovies(transformedData);
+			});
+	};
+
+	return (
+		<React.Fragment>
+			<section>
+				<button onClick={fetchMovieHandler}>Fetch Movies</button>
+			</section>
+			<section>
+				<MoviesList movies={movies} />
+			</section>
+		</React.Fragment>
+	);
 }
 
 export default App;
