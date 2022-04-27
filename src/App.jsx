dbc350fea62cf094b1694d67ffdb2a0355516148
Azie Melza Pratama <aziemp66@gmail.com>
2022-04-27 23:54:36 +0700

    Changing to Async Await

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,22 @@ function App() {
 	const [isLoading, setIsLoading] = useState(false);
 	const [movies, setMovies] = useState([]);
 
-	const fetchMovieHandler = () => {
+	const fetchMovieHandler = async () => {
 		setIsLoading(true);
-		fetch("https://swapi.dev/api/films/")
-			.then((response) => {
-				return response.json();
-			})
-			.then((data) => {
-				const transformedData = data.results.map((item) => {
-					return {
-						id: item.episode_id,
-						title: item.title,
-						openingText: item.opening_crawl,
-						releaseDate: item.release_date,
-					};
-				});
-				setMovies(transformedData);
-				setIsLoading(false);
-			});
+		const response = await fetch("https://swapi.dev/api/films");
+		const data = await response.json();
+
+		const transformedData = data.results.map((item) => {
+			return {
+				id: item.episode_id,
+				title: item.title,
+				openingText: item.opening_crawl,
+				releaseDate: item.release_date,
+			};
+		});
+
+		setMovies(transformedData);
+		setIsLoading(false);
 	};
 
 	return (
